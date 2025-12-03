939afca23d015deedd953e320fdb15bf2df36486
Son Doan Le Trung <sondoanletrung@gmail.com>
2025-12-03 23:36:15 +0700

    Update annotation_by_SingleR.R

--- R_code/annotation_by_SingleR.R
+++ R_code/annotation_by_SingleR.R
@@ -1,23 +1,31 @@
-# Automatic cell type annotation by SingleR
+# annotation_by_SingleR.R
+# Automatic cell type annotation using SingleR and celldex references
+
+
 library(Seurat)
 library(SingleR)
 library(celldex)
 library(SingleCellExperiment)
 
+
 annotate_cells <- function(sr_obj, ref_data = NULL) {
-  #install HumanPrimaryCellAtlasData for annotation
-  if (is.null(ref_data)) {
-    ref_data <- HumanPrimaryCellAtlasData()
-  }
-  
-  #Seurat object -> SingleCellExperiment object
-  sce <- as.SingleCellExperiment(sr_obj)
-  
-  #SingleR
-  pred.main <- SingleR(test = sce, ref = ref_data, labels = ref_data$label.main)
-  
-  #Add SingleR labels to Seurat
-  sr_obj$SingleR.labels <- pred.main$labels
-  
-  return(sr_obj)
+# Use HumanPrimaryCellAtlasData if no reference provided
+if (is.null(ref_data)) {
+ref_data <- HumanPrimaryCellAtlasData()
+}
+
+
+# Convert Seurat -> SingleCellExperiment
+sce <- as.SingleCellExperiment(sr_obj)
+
+
+# Run SingleR (assign main labels)
+pred.main <- SingleR(test = sce, ref = ref_data, labels = ref_data$label.main)
+
+
+# Attach SingleR labels to Seurat metadata
+sr_obj$SingleR.labels <- pred.main$labels
+
+
+return(sr_obj)
 }
